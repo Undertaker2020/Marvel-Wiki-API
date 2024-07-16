76bf15348bdd7383a514bd632e1dcd7faf296c36
Ilya <gashimovilya2018@gmail.com>
2024-07-16 06:26:26 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,50 @@
 import Header from "./Components/Header.jsx";
-
+import {Component} from "react";
 import RandomChar from "./Components/RandomChar.jsx";
-
-import './style.min.css'
 import CharContent from "./Components/CharContent.jsx";
+import CharInfo from "./Components/CharInfo.jsx";
+import './style.min.css'
 
-function App() {
-    return (
-        <div className="app">
-            <Header/>
-            <main>
-                <RandomChar/>
-                <CharContent/>
-                <img className="bg-decoration" src="../src/Image/vision.png" alt="vision"/>
-            </main>
-        </div>
+class App extends Component {
+    state =  {
+        showRandomChar: true,
+        selectedChar: null,
+    }
 
-    );
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <Header/>
+                <main>
+                    {this.state.showRandomChar ? <RandomChar/> : null}
+                    <button onClick={this.toggleRandomChar}>Click me</button>
+                    <div className="char__content">
+                        <CharContent charId={this.state.selectedChar} />
+                        {/*<CharContent onCharSelected={this.onCharSelected}/>*/}
+                        <CharInfo charId={this.state.selectedChar}/>
+                    </div>
+
+
+                    <img className="bg-decoration" src="../src/Image/vision.png" alt="vision"/>
+                </main>
+            </div>
+
+        );
+    }
 }
 
 export default App
--- src/Components/CharContent.jsx
+++ src/Components/CharContent.jsx
@@ -1,119 +1,81 @@
-function CharContent() {
-    return (
-        <div className="char__content">
+import {Component} from "react";
+import MarvelService from "../Services/MarvelService.js";
+import ErrorMessage from "./ErrorMessage.jsx";
+import Spinner from "./Spinner.jsx";
+
+
+class CharContent extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    state = {
+        charList: [],
+        loading: true,
+        error: false,
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.marvelService
+            .getAllCharacters()
+            .then(this.onCharListLoaded)
+            .catch(this.onError);
+    }
+
+    onCharListLoaded = (charList) => {
+        this.setState({charList, loading: false});
+    }
+    onError = () => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+
+    renderItems(arr) {
+        const items = arr.map((item) => {
+            let imgStyle = {'objectFit': 'cover'};
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit': 'unset'};
+            }
+            return (
+                <li className="char__item"
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
+                    <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                    <div className="char__name">{item.name}</div>
+                </li>
+            )
+        });
+        return (
+            <ul className="char__grid">
+                {items}
+            </ul>
+        )
+    }
+
+
+    render() {
+        const {charList, loading, error} = this.state;
+        const items = this.renderItems(charList);
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
+        return (
             <div className="char__list">
-                <ul className="char__grid">
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item char__item_selected">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                    <li className="char__item">
-                        <img src="../src/Image/abyss.jpg" alt="abyss"/>
-                        <div className="char__name">Abyss</div>
-                    </li>
-                </ul>
+                {errorMessage}
+                {spinner}
+                {content}
                 <button className="button button__main button__long">
                     <div className="inner">load more</div>
                 </button>
             </div>
-
-            <div className="char__info">
-                <div className="char__basics">
-                    <img src="../src/Image/thor.jpeg" alt="abyss"/>
-                    <div>
-                        <div className="char__info-name">thor</div>
-                        <div className="char__btns">
-                            <a href="#" className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href="#" className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="char__descr">
-                    In Norse mythology, Loki is a god or jötunn (or both). Loki is the son of Fárbauti and Laufey, and
-                    the brother of Helblindi and Býleistr. By the jötunn Angrboða, Loki is the father of Hel, the wolf
-                    Fenrir, and the world serpent Jörmungandr. By Sigyn, Loki is the father of Nari and/or Narfi and
-                    with the stallion Svaðilfari as the father, Loki gave birth—in the form of a mare—to the
-                    eight-legged horse Sleipnir. In addition, Loki is referred to as the father of Váli in the Prose
-                    Edda.
-                </div>
-                <div className="char__comics">Comics:</div>
-                <ul className="char__comics-list">
-                    <li className="char__comics-item">
-                        All-Winners Squad: Band of Heroes (2011) #3
-                    </li>
-                    <li className="char__comics-item">
-                        Alpha Flight (1983) #50
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #503
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing Spider-Man (1999) #504
-                    </li>
-                    <li className="char__comics-item">
-                        AMAZING SPIDER-MAN VOL. 7: BOOK OF EZEKIEL TPB (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Amazing-Spider-Man: Worldwide Vol. 8 (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Asgardians Of The Galaxy Vol. 2: War Of The Realms (Trade Paperback)
-                    </li>
-                    <li className="char__comics-item">
-                        Vengeance (2011) #4
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1963) #1
-                    </li>
-                    <li className="char__comics-item">
-                        Avengers (1996) #1
-                    </li>
-                </ul>
-                <p className="char__select">Please select a character to see information</p>
-                <div className="skeleton">
-                    <div className="pulse skeleton__header">
-                        <div className="pulse skeleton__circle"></div>
-                        <div className="pulse skeleton__mini"></div>
-                    </div>
-                    <div className="pulse skeleton__block"></div>
-                    <div className="pulse skeleton__block"></div>
-                    <div className="pulse skeleton__block"></div>
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default CharContent;
